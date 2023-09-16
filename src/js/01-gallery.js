f66a72b229dc877d6acecb27d685dc03a549cd80
ArturBaur <artur.baur@gmail.com>
2023-09-16 23:31:18 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryHTML = galleryItems
+  .map(
+    item => `
+    <a class="gallery__item" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>`
+  )
+  .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryHTML);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+lightbox.on('show.simplelightbox', () => {
+  document.addEventListener('keydown', escapePress);
+});
+
+lightbox.on('close.simplelightbox', () => {
+  document.removeEventListener('keydown', escapePress);
+});
+
+function escapePress(eventKey) {
+  if (eventKey.key === 'Escape') {
+    lightbox.close();
+  }
+}
