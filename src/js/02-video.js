7e2a9343fd8ec85918868dc5a76a59f0315b21a8
ArturBaur <artur.baur@gmail.com>
2023-09-16 23:42:39 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(parseFloat(savedTime));
+  }
+});
